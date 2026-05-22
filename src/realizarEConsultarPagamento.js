3396f5b1177723c297343551862521c242253694
Bruna Gomes <bruna.ckgomes@gmail.com>
2026-05-22 18:11:19 -0300

    Inserido melhorias nos testes e removido campos desnecessários

--- src/realizarEConsultarPagamento.js
+++ src/realizarEConsultarPagamento.js
@@ -1,5 +1,10 @@
 /*
-Crie uma classe que possua dois métodos: um para realizar pagamento e outro para consultar o último pagamento. Pagamentos serão armazenados como objetos Javascript dentro de uma lista de pagamentos. Cada pagamento terá as propriedades: Código de Barras, Empresa e Valor. Quando um pagamento for realizado e o valor for maior que 100.00, o pagamento também terá a propriedade categoria como 'cara', caso contrário, a propriedade categoria ficará como 'padrão'. O método de consultar trará apenas o último pagamento.
+Crie uma classe que possua dois métodos: um para realizar pagamento e outro para consultar o 
+último pagamento. Pagamentos serão armazenados como objetos Javascript dentro de uma lista de 
+pagamentos. Cada pagamento terá as propriedades: Código de Barras, Empresa e Valor. 
+Quando um pagamento for realizado e o valor for maior que 100.00, o pagamento também terá 
+a propriedade categoria como 'cara', caso contrário, a propriedade categoria ficará como 'padrão'. 
+O método de consultar trará apenas o último pagamento.
   
   ex. 
   const servicoDePagamento = new ServicoDePagamento();
